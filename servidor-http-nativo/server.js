cac8ba794c373e58f36b4e244aeb522be5d3bde7
Kauan P. <kauanprado279@gmail.com>
2026-08-17 19:57:15 +0000

    EX1: 3 Rotas AULA 17/08

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -1,3 +1,4 @@
+/*
 const http = require('node:http')
 
 const porta = 3000
@@ -19,3 +20,41 @@ server.listen(porta, ()=> {
     console.log(`Servidor ouvindo na porta ${porta}`)
 });
 //objetos são instancias de uma classe ou qualquer variavel do javascript
+*/
+
+import http from 'node:http'
+
+import { url } from 'node:inspector'
+import { URL } from 'node:url'
+
+const port = 3000
+
+const status ={ 
+    "status": "ok",
+    "date": new Date().toISOString()
+}
+
+const produtos = [
+    { id: 1, nome: "Sabonete" },
+    { id: 2, nome: "Monitor" },
+    { id: 3, nome: "Cadeira Gamer" }
+]
+
+const server = http.createServer((req, res) => {
+    const urlObj = new URL(req.url, `http://${req.headers.host}`)
+    res.statuscode = 200;
+    res.setHeader('Content-Type', 'application/json')
+
+    if (req.method == "GET" && urlObj.pathname == "/contato"){
+        return res.end(JSON.stringify({"numero_telefone": "67 99999-9999", "endereco": "Rua da Alegria, 99"}))
+    }
+
+    if (req.method == "GET" && urlObj.pathname == "/produtos") {
+        return res.end(JSON.stringify(produtos));
+    }
+
+    res.end(JSON.stringify({"data": "Página Inicial"}))
+})
+server.listen(port, () =>{
+    console.log(`Servidor ouvindo na porta ${port}`)
+})
