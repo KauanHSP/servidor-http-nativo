b2d0cd191dafa70214711e68e7cd29926f653f6c
Kauan P. <kauanprado279@gmail.com>
2026-08-17 19:12:10 +0000

    EX4: Comentario linha 15 e anotações e explicações na aula

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -2,18 +2,20 @@ const http = require('node:http')
 
 const porta = 3000
 
-const server = http.createServer();
+const server = http.createServer();// metodo que cria o servidor
 
-server.on('request', (req, res) => {
+server.on('request', (req, res) => {//Arrow function 
     console.log(`Requisição recebida! ${req.method} ${req.url}`);
 
     console.log(new Date().toISOString())
 
-    res.statusCode = 201
-    res.setHeader('Content-Type', 'application/json; charset=utf-8');
-    res.end(JSON.stringify({ status: "ok" }));    
+    res.statusCode = 201// status 
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');// tipo que aparece no cabeçario
+    res.end(JSON.stringify({ "status": "ok" })); //converte com JSON
+    //EX4: Quando nós excluimos a linha res.end(), o navegador fica carregando eternamente a página e trava ele, pois ela fecha o ciclo "req e res"
 });
 
 server.listen(porta, ()=> {
     console.log(`Servidor ouvindo na porta ${porta}`)
 });
+//objetos são instancias de uma classe ou qualquer variavel do javascript
